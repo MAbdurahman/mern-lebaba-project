56a3777721324f2276285e865f2f27ff05e7f6fa
MAbdurahman <mdbdrrhm@gmail.com>
2024-10-19 03:34:37 -0500

    Add basic structure and styles of sections/CategorySection.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import BannerComponent from '../components/home/BannerComponent.jsx';
+import CategorySection from '../sections/CategorySection.jsx';
 
 export default function HomePage() {
 
    return (
       <>
          <BannerComponent />
+         <CategorySection />
       </>
 
    );
