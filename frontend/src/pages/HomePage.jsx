5a6eb67c6f284291bc17aa71cacb132610cf549b
MAbdurahman <mdbdrrhm@gmail.com>
2024-10-30 03:10:18 -0500

    Update styles for layouts/FooterLayout.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,9 @@ import BannerComponent from './../components/home/BannerComponent';
 import CategorySection from './../sections/CategorySection';
 import HeroSection from './../sections/HeroSection';
 import TrendingProductsSection from './../sections/TrendingProductsSection';
+import DealsSection from './../sections/DealsSection';
+import PromotionBannerSection from './../sections/PromotionBannerSection';
+import BlogSection from '../sections/BlogSection';
 
 export default function HomePage() {
 
@@ -12,6 +15,9 @@ export default function HomePage() {
          <CategorySection />
          <HeroSection />
          <TrendingProductsSection />
+         <DealsSection />
+         <PromotionBannerSection />
+         <BlogSection />
       </>
 
    );
