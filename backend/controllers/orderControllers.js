9f959fb8624afd0ceb6e7fa44f9c350f27f4b6d2
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2024-12-02 05:12:53 -0600

    Add complete function confirmPayment to backend/controllers/orderControllers.js

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -2,6 +2,9 @@ import Order from './../models/orderModel.js';
 import verifyTokenMiddleware from '../middlewares/verifyTokenMiddleware.js';
 import verifyAdminMiddleware from '../middlewares/verifyAdminMiddleware.js';
 import {messageHandler} from '../utils/messageHandlerUtils.js';
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
 
 export const createCheckoutSession = async (req, res) => {
    const {products} = req.body;
@@ -36,10 +39,50 @@ export const createCheckoutSession = async (req, res) => {
 }//end of createCheckoutSession Function
 
 export const confirmPayment = async (req, res) => {
+   const {session_id} = req.body;
 
-   res.status(20).send({
-      message: 'Payment was successfully confirmed!'
-   })
+   try {
+      const session = await stripe.checkout.sessions.retrieve(session_id, {
+         expand: ['line_items', 'payment_intent'],
+      });
+
+      const paymentIntentId = session.payment_intent.id;
+
+      let order = await Order.findOne({ orderId: paymentIntentId });
+
+      if (!order) {
+         const lineItems = session.line_items.data.map((item) => ({
+            productId: item.price.product,
+            quantity: item.quantity,
+         }));
+
+         const amount = session.amount_total / 100;
+
+         order = new Order({
+            orderId: paymentIntentId,
+            products: lineItems,
+            amount: amount,
+            email: session.customer_details.email,
+            status:
+               session.payment_intent.status === 'succeeded' ? 'pending' : 'failed',
+         });
+      } else {
+         order.status =
+            session.payment_intent.status === 'succeeded' ? 'pending' : 'failed';
+      }
+
+      await order.save();
+
+      res.status(200).send({
+         message: 'Payment was successfully confirmed!',
+         order,
+
+      });
+   } catch(err) {
+      console.log('error confirming payment', err.message);
+      messageHandler(res, 'Error confirming payment!', false, 500);
+   }
+
 }//end of confirmPayment Function
 
 export const getAllOrders = async (req, res) => {
